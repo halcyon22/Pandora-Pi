95fdaeb9f55c3622c0619a99ae70412b0e0c0ee3
Mike Partridge <halcyon22@gmail.com>
2013-07-17 22:42:59 -0500

    ButtonWatcher refactoring
    rearrange imports
    extract & change button channels - avoid problems with SDA, SCL
    remove unused variables

--- scripts/ButtonWatcher.py
+++ scripts/ButtonWatcher.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python
  
-import sys, serial, time, os, socket
+import sys, serial, time, os, socket, pickle
+import RPi.GPIO as GPIO
 from time import sleep
 from random import randint
 from subprocess import Popen
 import pandoraUtils
-import pickle
-import RPi.GPIO as GPIO
+
+BUTTON1 = 4
+BUTTON2 = 17
+BUTTON3 = 21
+BUTTON4 = 22
+BUTTON5 = 10
+BUTTON6 = 9
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.IN) # Skip Song
-GPIO.setup(3, GPIO.IN) # Change Station
-GPIO.setup(4, GPIO.IN) # Vol -
-GPIO.setup(17, GPIO.IN) # Vol +
-GPIO.setup(27, GPIO.IN) # Play/Pause
-GPIO.setup(22, GPIO.IN) # Menu
+GPIO.setup(BUTTON1, GPIO.IN)
+GPIO.setup(BUTTON2, GPIO.IN)
+GPIO.setup(BUTTON3, GPIO.IN)
+GPIO.setup(BUTTON4, GPIO.IN)
+GPIO.setup(BUTTON5, GPIO.IN)
+GPIO.setup(BUTTON6, GPIO.IN)
 
-scripts_folder_location = '/home/pi/.config/pianobar/scripts/'
-fifo_folder_location = '/home/pi/.config/pianobar/ctl'
+fifo_folder_location = './ctl'
 
 LCD = serial.Serial('/dev/ttyAMA0', 9600)
 
-total_stations = 32
+total_stations = 10
 current_station = 7
 
-allow_lcd_update = True
-forceUpdate = False
-
 playing_stream = "Playing"
-
 current_screen = "default"
 
 def main():
@@ -41,40 +42,41 @@ def main():
 	
 	sleep(4)
 	
-	pandoraUtils.parseAndWrite()
+	#pandoraUtils.parseAndWrite()
 
 	while True:
 
 		# Default Page
 
-		if ( GPIO.input(2) == False  and current_screen == "default"):
+		if (GPIO.input(BUTTON1) == False  and current_screen == "default"):
 			
 			pandoraUtils.log('Button 1 - Skip Song')
 			pandoraUtils.writeToLCD("Skipping", "Song")
 			os.system('echo "n" >> ' + fifo_folder_location)
 
-		if ( GPIO.input(3) == False  and current_screen == "default"):
+
+		if (GPIO.input(BUTTON2) == False  and current_screen == "default"):
 
 			pandoraUtils.log('Button 2 - Skip Station')
 			pandoraUtils.writeToLCD("Next", "Station")
 			current_station = randint(1,total_stations)
+			pandoraUtils.log('new station'+str(current_station))
 			os.system('echo "s' + str(current_station) + '" >> ' + fifo_folder_location)
 
 			parseAndWrite(8, True)
-
 			parseAndWrite(3)
 
 
-		if ( GPIO.input(4)== False  and current_screen == "default"):
+		if (GPIO.input(BUTTON3) == False  and current_screen == "default"):
 
 			pandoraUtils.log('Button 3 - Vol Down')
 			pandoraUtils.writeToLCD("Volume", "Down")
 			os.system('echo "((((" >> ' + fifo_folder_location)
 
-			sleparseAndWriteep(2)
+			parseAndWrite(2)
 
 
-		if ( GPIO.input(17)== False  and current_screen == "default"):
+		if (GPIO.input(BUTTON4) == False  and current_screen == "default"):
 
 			pandoraUtils.log('Button 4 - Vol Up')
 			pandoraUtils.writeToLCD("Volume", "Up")
@@ -83,7 +85,7 @@ def main():
 			parseAndWrite(2)
 
 
-		if ( GPIO.input(27)== False  and current_screen == "default"):
+		if (GPIO.input(BUTTON5) == False  and current_screen == "default"):
 
 			pandoraUtils.log('Button 5 - Play/Pause')
 
@@ -98,7 +100,7 @@ def main():
 			os.system('echo "p" >> ' + fifo_folder_location)
 
 			
-		if ( GPIO.input(22)== False  and current_screen == "default"):
+		if (GPIO.input(BUTTON6) == False  and current_screen == "default"):
 
 			pandoraUtils.log('Button 6 - Menu')
 			
@@ -110,7 +112,7 @@ def main():
 
 		# Menu Page 1
 
-		if ( GPIO.input(2) == False  and current_screen == "menupg1"):
+		if (GPIO.input(BUTTON1) == False  and current_screen == "menupg1"):
 
 			pandoraUtils.log('Button 6 - Menu - Sub 1 - Like')
 			pandoraUtils.writeToLCD("Liking", pandoraUtils.getShared("song"))
@@ -119,7 +121,7 @@ def main():
 			parseAndWrite(2)
 			
 
-		if ( GPIO.input(3) == False  and current_screen == "menupg1"):
+		if (GPIO.input(BUTTON2) == False  and current_screen == "menupg1"):
 
 			pandoraUtils.log('Button 6 - Menu - Sub 2 - DisLike')
 			pandoraUtils.writeToLCD("Disliking", pandoraUtils.getShared("song"))
@@ -127,7 +129,7 @@ def main():
 
 			parseAndWrite(2)
 
-		if ( GPIO.input(4) == False  and current_screen == "menupg1"):
+		if (GPIO.input(BUTTON3) == False  and current_screen == "menupg1"):
 
 			pandoraUtils.log('Button 6 - Menu - Sub 3 - IP Address')
 			getIPAddress()
@@ -135,27 +137,27 @@ def main():
 			parseAndWrite(5)
 
 
-		if ( GPIO.input(17) == False  and current_screen == "menupg1"):
+		if ( GPIO.input(BUTTON4) == False  and current_screen == "menupg1"):
 
 			pandoraUtils.writeToLCD("1:Prev Pg 2:Cur St", "3:Off 4:Next Pg")
 			current_screen = "menupg2"
 
 		# Menu Page 2
 
-		if ( GPIO.input(2) == False  and current_screen == "menupg2"):
+		if (GPIO.input(BUTTON1) == False  and current_screen == "menupg2"):
 
 			pandoraUtils.log('Button 6 - Menu - Sub 1 - Menu Pg 1')
 			pandoraUtils.writeToLCD("1:Like 2:Dislike", "3:IP 4:Next Pg")
 			current_screen = "menupg1"
 
-		if ( GPIO.input(3) == False  and current_screen == "menupg2"):
+		if (GPIO.input(BUTTON2) == False  and current_screen == "menupg2"):
 
 			pandoraUtils.log('Button 6 - Menu - Sub 2 - Station Name')
 			pandoraUtils.writeToLCD(pandoraUtils.getShared("stationName"), "")
 
 			parseAndWrite(2)
 
-		# if ( GPIO.input(4) == False  and current_screen == "menupg2"):
+		# if (GPIO.input(BUTTON3) == False  and current_screen == "menupg2"):
 
 		# 	pandoraUtils.log('Button 6 - Menu - Sub 3 - Shutdown')
 		# 	pandoraUtils.writeToLCD("Shutting Down", "Thanks")
@@ -167,7 +169,7 @@ def main():
 		# 	pandoraUtils.writeToLCD("Pandora Pi: OFF", "1: Turn ON")
 		# 	current_screen = "off"
 
-		# if ( GPIO.input(2) == False  and current_screen == "off"):
+		# if ( GPIO.input(BUTTON1) == False  and current_screen == "off"):
 
 		# 	pianobarProcess = Popen('sudo -u pi pianobar', shell=True)
 
