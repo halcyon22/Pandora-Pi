9550d6a0412fef1edf5a403898178a41e1315b2d
Mike Partridge <halcyon22@gmail.com>
2013-07-27 00:35:08 -0500

    switch to previous station on start

--- scripts/eventReciever.py
+++ scripts/eventReciever.py
@@ -6,44 +6,50 @@ import pandoraUtils
 
 if __name__ == "__main__":
 
-	# Read event type from command arguments
-	if len(sys.argv) < 2:
-		pandoraUtils.log("Error reading event type from command arguments")
+    # Read event type from command arguments
+    if len(sys.argv) < 2:
+        pandoraUtils.log("Error reading event type from command arguments")
 
-	event_type = sys.argv[1]
-	pandoraUtils.log("event_type="+event_type)
+    event_type = sys.argv[1]
+    pandoraUtils.log("event_type="+event_type)
 
-	# Read parameters from input
-	params = {}
-	
-	for s in sys.stdin.readlines():
-		param, value = s.split("=", 1)
-		params[param.strip()] = value.strip()
+    # Read parameters from input
+    params = {}
+    
+    for s in sys.stdin.readlines():
+        param, value = s.split("=", 1)
+        params[param.strip()] = value.strip()
 
-	# Handle specific events
-	if event_type == "songstart":
+    # Handle specific events
+    if event_type == "songstart":
 
-		info = {}
-		info["song"] = params["title"]
-		info["artist"] = params["artist"]
-		info["album"] = params["album"]
-		info["stationCount"] = params["stationCount"]
-		info["stationName"] = params["stationName"]
+        info = {}
+        info["song"] = params["title"]
+        info["artist"] = params["artist"]
+        info["album"] = params["album"]
+        info["stationCount"] = params["stationCount"]
+        info["stationName"] = params["stationName"]
 
-		stations = []
+        stations = []
 
-		for i in range(0, int(params["stationCount"])):
-			stations.append(params["station"+str(i)])
+        for i in range(0, int(params["stationCount"])):
+            stations.append(params["station"+str(i)])
 
-		info["stations"] = stations
+            if params["station"+str(i)] == params["stationName"]:
+                info["stationNumber"] = i
 
-		pandoraUtils.setShared(info)
-		pandoraUtils.parseAndWrite()
+        info["stations"] = stations
 
-	elif event_type == "songfinish":
-		pass
-	elif event_type == "usergetstations":
-		pass
-	elif event_type == "userlogin":
-		pandoraUtils.writeToLCD("Logging in...")
+        pandoraUtils.setShared(info)
+        pandoraUtils.parseAndWrite()
+
+    elif event_type == "songfinish":
+        pass
+
+    elif event_type == "usergetstations":
+        previous_station = pandoraUtils.getShared("stationNumber")
+        pandoraUtils.sendCommand('s'+str(previous_station))
+
+    elif event_type == "userlogin":
+        pandoraUtils.writeToLCD("Logging in...")
 
